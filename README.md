bdeec328ecb55890770f301efefb4631b7750778
Jaaz7 <jaazielvale@hotmail.com>
2023-07-16 19:51:16 +0200

    added source links to README.md, coded the rendered table for the expenses and started to code the app header numbers updating, meaning the total, income and expenses.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 background image: https://www.freepik.com/free-vector/piggy-bank-with-gold-coins-3d-cartoon-style-icon-saving-investing-accumulating-money-flat-vector-illustration-income-finance-banking-wealth-currency-concept_29119345.htm#page=2&query=piggy&position=38&from_view=search&track=sph
+
+aid:
+https://www.youtube.com/watch?v=rhzKDrUiJVk
+https://gist.github.com/phegman/755bd41197cd59c435db56c367d9d0fd
+
+https://www.geeksforgeeks.org/how-to-parse-float-with-two-decimal-places-in-javascript/
+https://www.tutorialspoint.com/extract-a-number-from-a-string-using-javascript#
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,82 @@
 // Only run the js code when the DOM has completely loaded
 document.addEventListener('DOMContentLoaded', function () {
-    let submit = document.getElementById('submit');
-
-    if (submit) {
-        button.addEventListener('click', function () {
-            if (this.getAttribute('data-type') === 'submit') {
-                checkAnswer();
-            } else {
-                let gameType = this.getAttribute('data-type');
-                runGame(gameType);
-            }
-        })
-    }
-    document.getElementById('name').addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
-            runTable();
-        }
+    document.getElementsByTagName('form')[0].addEventListener('submit', function (e) {
+        e.preventDefault();
+        runTable();
     })
-
-    runGame('add');
+    document.getElementById('name').focus();
+    setMaxDate();
 })
 
+function runTable() {
+    const values = [];
+    const name = document.getElementById('name').value;
+    const date = document.getElementById('date').value;
+    const expense = parseFloat(document.getElementById('amount').value).toFixed(2);
+    if (expense >= 100000) {
+        alert('Value number is too big.');
+    } else {
+        valueConfig();
+        const list = document.getElementById('list');
+        list.innerHTML = `
+    <table id="table">
+        <thead>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Date
+                </th>
+                <th>
+                    Value
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    ${name}
+                </td>
+                <td>
+                    ${date}
+                </td>
+                <td>
+                    ${expense}
+                </td>
+            </tr>
+        </tbody>
+    </table>`;
+    }
+    document.getElementById('name').value = '';
+    document.getElementById('name').focus();
+    document.getElementById('date').value = null;
+    document.getElementById('amount').value = '';
+}
 
-/**
- * Allow only numbers and one dot as a decimal separator
- * in enter expense input
- */
-function validate(s) {
-    var rgx = /^[0-9]*\.?[0-9]*$/;
-    return s.match(rgx);
+function setMaxDate() {
+    const todayDate = new Date();
+    const year = todayDate.getFullYear();
+    let month = todayDate.getMonth() + 1;
+    let day = todayDate.getDate();
+    if (month < 10) {
+        month = '0' + month;
+    }
+    if (day < 10) {
+        day = '0' + day;
+    }
+    const maxDate = year + '-' + month + '-' + day;
+    document.getElementById('date').setAttribute('max', maxDate);
+}
+
+function valueConfig() {
+    const entryValue = parseFloat(document.getElementById('amount').value).toFixed(2);
+    const innerTotalValue = document.getElementById('total').innerHTML;
+    const totalValue = parseFloat(innerTotalValue.match((/(\d+)/)));
+    const innerIncome = document.getElementById('income').innerHTML;
+    const innerExpense = document.getElementById('expense').innerHTML;
+    console.log(entryValue);
+    console.log(totalValue);
+    console.log(income);
+    console.log(expense);
 }
