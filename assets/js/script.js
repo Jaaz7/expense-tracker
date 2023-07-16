494ac7b3691117c5a8a6c56ffacc49f583ade77a
Jaaz7 <jaazielvale@hotmail.com>
2023-07-16 13:47:08 +0200

    added style to the buttons, text and  inputs, added background colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+// Only run the js code when the DOM has completely loaded
+document.addEventListener('DOMContentLoaded', function () {
+    let submit = document.getElementById('submit');
+
+    if (submit) {
+        button.addEventListener('click', function () {
+            if (this.getAttribute('data-type') === 'submit') {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute('data-type');
+                runGame(gameType);
+            }
+        })
+    }
+    document.getElementById('name').addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            runTable();
+        }
+    })
+
+    runGame('add');
+})
+
+
 /**
  * Allow only numbers and one dot as a decimal separator
  * in enter expense input
