4cfd1674cb9b69a623cf3a5b74536a301d8ce557
Jaaz7 <jaazielvale@hotmail.com>
2023-07-17 15:35:38 +0200

    created a function to display an array as a table when clicking on submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementsByTagName('form')[0].addEventListener('submit', function (e) {
         e.preventDefault();
-        runTable();
+        start();
     })
     document.getElementById('name').focus();
     setMaxDate();
 })
 
-function runTable() {
-    const values = [];
-    const name = document.getElementById('name').value;
-    const date = document.getElementById('date').value;
+/**
+ * when the submit button is clicked, this reads the inserted values
+ * and checks if it can go forward to render the table 
+ */
+function start() {
     const amount = Math.round(document.getElementById('amount').value * 1e2) / 1e2;
     const amountString = String(document.getElementById('amount').value);
     if (amount >= 100000) {
@@ -20,25 +21,7 @@ function runTable() {
         alert("Negative numbers and special characters aren't allowed.");
     } else {
         valueConfig();
-        const list = document.getElementById('list');
-        list.innerHTML = `
-    <table id="table">
-        <thead>
-            <tr>
-                <th>
-                    Name
-                </th>
-                <th>
-                    Date
-                </th>
-                <th>
-                    Value
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-        </tbody>
-    </table>`;
+        addTable();
     }
     document.getElementById('name').value = '';
     document.getElementById('name').focus();
@@ -46,6 +29,9 @@ function runTable() {
     document.getElementById('amount').value = '';
 }
 
+/**
+ *only allow the date to be selected up until the current day
+ */
 function setMaxDate() {
     const todayDate = new Date();
     const year = todayDate.getFullYear();
@@ -61,6 +47,9 @@ function setMaxDate() {
     document.getElementById('date').setAttribute('max', maxDate);
 }
 
+/**
+ * update the numbers on the app header
+ */
 function valueConfig() {
     //Getting the value from the form
     const entryValue = Math.round(document.getElementById('amount').value * 1e2) / 1e2;
@@ -84,6 +73,104 @@ function valueConfig() {
     }
 }
 
-function addRow() {
+/**
+ * capture the inserted data and set to an array so it's prepared
+ * to show inside the table with the function addTable()
+ */
+function displayTableData() {
 
 }
+
+function addTable() {
+    let values = [
+        {
+            number: 1,
+            name: 'groceries',
+            date: new Date(),
+            value: 30,
+            type: 'expense'
+        },
+        {
+            number: 2,
+            name: 'haircut',
+            date: new Date(),
+            value: 15,
+            type: 'expense'
+        },
+        {
+            number: 3,
+            name: 'salary',
+            date: new Date(date).toLocaleDateString(),
+            value: 3000,
+            type: 'income'
+        },
+    ];
+    let html = `
+        <table id="table">
+            <thead>
+                <tr>
+                    <th>
+                        No.
+                    </th>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Date
+                    </th>
+                    <th>
+                        Value
+                    </th>
+                    <th>
+                        Type
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+            `;
+    for (let i = 0; i < values.length; i++) {
+        let idNum = i + 1;
+        html += '<tr>';
+        html += '<td>' + idNum + '</td>';
+        html += '<td>' + values[i].name + '</td>';
+        html += '<td>' + values[i].date + '</td>';
+        html += '<td>' + values[i].value + '</td>';
+        html += '<td>' + values[i].type + '</td>';
+    }
+    html += `
+            </tr>
+            </tbody>
+            </table>
+            `;
+    const list = document.getElementById('list').innerHTML = html;
+
+}
+
+// function addRow() {
+//     let values = [];
+
+//     for (let value of values) {
+//         let object = {};
+//         let number = 0;
+//         object.id = number + 1;
+//         object.name = document.getElementById('name').value;
+//         object.date = document.getElementById('date').toLocaleDateString();
+//         object.amount = Math.round(document.getElementById('amount').value * 1e2) / 1e2;
+//         if (document.querySelector('#check').checked) {
+//             object.type = 'Income';
+//         } else {
+//             object.type = 'Expense';
+//         }
+//         console.log(values);
+//         let rowHtml = `
+//         <tr>
+//             <td>${value.name}</td>
+//             <td>${value.date}</td>
+//             <td>${value.amount}</td>
+//             <td>${value.type}</td>
+//         </tr>
+//         `;
+//         const tbody = document.getElementsByTagName('tbody')[0];
+//         tbody.innerHTML += rowHtml;
+//     }
+// }
